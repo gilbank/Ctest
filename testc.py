3d17e4915cd754d7e5e0b84c472ea30b500d2992
David Gilbank <gilbank@saao.ac.za>
2012-10-10 12:26:51 +0200

    adding code

---
+++ testc.py
@@ -0,0 +1,15 @@
+
+# test 1:
+# from:
+#
+# http://www.tutorialspoint.com/python/python_further_extensions.htm
+#
+# python setup.py  install
+
+from helloworld import helloworld
+
+helloworld()
+# okay!
+
+
+
